677079e046dfd49b87c0820e92e28057c0773a9f
Gustavo Meireles <gustavo.meireles@gmail.com>
2025-11-15 12:06:35 -0500

    chore: ncurc added to ui package

---
+++ packages/ui/.ncurc.js
@@ -0,0 +1,15 @@
+/** @type {import('npm-check-updates').RcOptions } */
+module.exports = {
+  /**
+    @param name    The name of the dependency.
+    @param semver  A parsed Semver array of the current version.
+      (See: https://git.coolaj86.com/coolaj86/semver-utils.js#semverutils-parse-semverstring)
+    @returns       True if the package should be excluded, false if it should be included.
+  */
+  reject: (name, semver) => {
+    if (name.includes('storybook')) {
+      return true
+    }
+    return false
+  }
+}
